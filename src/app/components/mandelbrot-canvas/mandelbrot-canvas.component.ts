409b8aa9d35cb339f3e4d18360206983e675f4c5
manuelmolinaf <manuelmolinaf@outlook.com>
2019-10-26 13:02:48 +0200

    added julia set functionality

--- src/app/components/mandelbrot-canvas/mandelbrot-canvas.component.ts
+++ src/app/components/mandelbrot-canvas/mandelbrot-canvas.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
-const WIDTH = 2500;
-const HEIGHT = 2500;
+const WIDTH = 1000;
+const HEIGHT = 1000;
 
 @Component({
-  // tslint:disable-next-line: component-selector
   selector: 'mandelbrot-canvas',
   templateUrl: './mandelbrot-canvas.component.html',
   styleUrls: ['./mandelbrot-canvas.component.css']
@@ -16,13 +15,20 @@ export class MandelbrotCanvasComponent implements OnInit {
 
   private ctx: CanvasRenderingContext2D;
 
-  public maxN:number = 500;
+  public maxN:number = 200;
 
   public minR:number = -1.5;
-  public maxR:number = 0.7;
+  public maxR:number = 1.5;
 
-  public minI:number = -1.0;
-  public maxI:number = 1.0;
+  public minI:number = -1.5;
+  public maxI:number = 1.5;
+
+  public cx = 0;
+  public cy = 0.8;
+
+  public isJuliaSet = 1;
+
+  public img:any;
 
 
   constructor() { }
@@ -30,6 +36,7 @@ export class MandelbrotCanvasComponent implements OnInit {
   ngOnInit() {
     this.ctx = this.canvas.nativeElement.getContext('2d');
     this.draw();
+    this.canvasToImage();
   }
 
   draw() {
@@ -40,12 +47,19 @@ export class MandelbrotCanvasComponent implements OnInit {
 
         let cr = this.mapToReal(x, WIDTH, this.minR, this.maxR);
         let ci = this.mapToImaginary(y, HEIGHT, this.minI, this.maxI);
-        let n = this.findMandelbrot(cr, ci, this.maxN);
+        let n = 0.0;
+        if(!this.isJuliaSet) {
+          n = this.findMandelbrot(cr, ci, this.maxN);
+        }
+        else {
+          n = this.findJuliaSet(cr, ci, this.maxN);
+        }
+        
 
         //coloring functions
-        let r = (n * Math.sin(30) % 256);
-        let g = (n * Math.acosh(n) % 256);
-        let b = ( n * 30 % 256);
+        let r = (n % 255);
+        let g = (n % 255);
+        let b = (n % 255);
 
         this.ctx.beginPath();
         this.ctx.rect(x, y, 1, 1);
@@ -79,19 +93,58 @@ export class MandelbrotCanvasComponent implements OnInit {
   findMandelbrot(cr:number, ci:number, maxIteration:number):number {
 
     let i = 0;
-    let zr = 0.0;
-    let zi = 0.0;
+   
+    let x = cr;
+    let y = ci;
 
-    while( i < maxIteration && zr * zr + zi * zi < 4.0) {
+    while( i < maxIteration) {
+      
+      let xx = x*x;
+      let yy = y*y;
+      let twoxy = 2.0 * x * y;
+      x = xx - yy + cr;
+      y = twoxy + ci;
 
-      let temp = zr * zr - zi * zi + cr;
-      zi = 2.0 * zr * zi + ci;
-      zr = temp;
+      if(xx*xx + yy*yy > 16.0) {
+        break;
+      }
       i++;
     }
-
 
     return i;
   }
 
+  findJuliaSet(cr:number, ci:number, maxIteration:number):number {
+
+    let i = 0;
+   
+    let x = cr;
+    let y = ci;
+
+    while( i < maxIteration) {
+      
+      let xx = x*x;
+      let yy = y*y;
+
+      if(xx*xx + yy*yy > 4.0) {
+        break;
+      }
+
+      let twoxy = 2.0 * x * y;
+      x = xx - yy + this.cx;
+      y = twoxy + this.cy;
+
+      
+      i++;
+    }
+   return i;
+  }
+
+
+  canvasToImage() {
+    let canvas = this.canvas.nativeElement;
+
+    this.img = canvas.toDataURL("image/png");
+
+  }
 }
